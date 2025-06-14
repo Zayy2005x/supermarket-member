b0c4d90795853d8cb5ca82cb372725e3fcd874ac
Zayy2005x <2781310592@qq.com>
2025-06-14 21:34:18 +0800

    角色权限分类常量

---
+++ src/main/java/com/zayy/supermarketmember/common/constant/RoleConstant.java
@@ -0,0 +1,10 @@
+package com.zayy.supermarketmember.common.constant;
+
+/**
+ * 角色分类常量
+ */
+public class RoleConstant {
+    public static final Byte SUPER_ADMINISTRATION = 0;  //超级管理员
+    public static final Byte ADMINISTRATION = 1;    //管理员
+    public static final Byte CUSTOMER = 2;  //顾客
+}
